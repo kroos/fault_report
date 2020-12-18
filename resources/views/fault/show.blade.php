7e4d2fe6eb7582d40ef096e3751df2c089ae829b
Kroos <dhiauddin@gmail.com>
2020-12-18 11:30:08 +0800

    - fix delete image on show fault

--- resources/views/fault/show.blade.php
+++ resources/views/fault/show.blade.php
@@ -165,4 +165,68 @@
 @endsection
 
 @section('js')
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// image delete
+$(document).on('click', '.delete_image', function(e){
+	var imgId = $(this).data('id');
+	SwalImageDelete(imgId);
+	e.preventDefault();
+});
+
+function SwalImageDelete(imgId){
+	swal.fire({
+		title: 'Are you sure?',
+		text: "Data will be deleted!",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Yes, delete it!',
+		showLoaderOnConfirm: true,
+
+		preConfirm: function() {
+			return new Promise(function(resolve) {
+				$.ajax({
+					type: 'DELETE',
+					url: '{{ url('faultImage') }}' + '/' + imgId,
+					data: {
+							_token : $('meta[name=csrf-token]').attr('content'),
+							id: imgId,
+					},
+					dataType: 'json'
+				})
+				.done(function(response){
+					swal.fire('Deleted!', response.message, response.status)
+					.then(function(){
+						window.location.reload(true);
+					});
+					//$('#disable_user_' + imgId).parent().parent().remove();
+				})
+				.fail(function(response){
+					var resp = response.responseJSON;
+					console.log(resp.errors);
+					var x = "";
+					for(i in resp.errors) {
+						x += '<p class="text-danger">' + resp.errors[i] + '</p>';
+					};
+					swal.fire({
+						title: 'Oops...',
+						html: x,
+						type: 'error',
+					});
+				})
+			});
+		},
+		allowOutsideClick: false
+	})
+	.then((result) => {
+		if (result.dismiss === swal.DismissReason.cancel) {
+			swal.fire('Cancelled', 'Your data is safe from delete', 'info')
+		}
+	});
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 @endsection
